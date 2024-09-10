ed4a7ff82b951c6ecb79fccffa263586e13a017e
Alvarowns <alvs.iosdev@gmail.com>
2024-09-10 12:10:50 +0200

    Initial Commit

---
+++ DuelStats MTG/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DuelStats MTG
+//
+//  Created by Alvaro Santos Orellana on 10/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DuelStats MTG/DuelStats_MTGApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuelStats_MTGApp.swift
+//  DuelStats MTG
+//
+//  Created by Alvaro Santos Orellana on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DuelStats_MTGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
